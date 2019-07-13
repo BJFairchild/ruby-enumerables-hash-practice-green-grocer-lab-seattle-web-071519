43f5703f3ad6254de161b8126fae0d978595e442
Brittan Fairchild <brittanfairchild@gmail.com>
2019-07-13 00:07:27 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,17 @@
 def consolidate_cart(cart)
-  # code here
+   my_cart = {}
+  
+  cart.each_with_index do |item, i|
+    item.each do |food, info|
+      if my_cart[food]
+        my_cart[food][:count] += 1
+      else
+        my_cart[food] = info
+        my_cart[food][:count] = 1
+      end
+    end
+  end
+  my_cart
 end
 
 def apply_coupons(cart, coupons)
